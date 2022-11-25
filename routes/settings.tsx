72b47929f0a405cbf3253852ae7418b743e32db3
ncolesummers <nsummers72@gmail.com>
2022-11-24 23:39:50 -0800

    time sample

--- routes/settings.tsx
+++ routes/settings.tsx
@@ -1,6 +1,7 @@
 import { State } from "../utils/state.ts";
 import { Handlers, PageProps } from "$fresh/server.ts";
 import { setCookie } from "$std/http/cookie.ts";
+import LocaleSelector from "../islands/LocaleSelector.tsx";
 
 export const handler: Handlers<State, State> = {
   GET(_req, ctx) {
@@ -12,9 +13,13 @@ export const handler: Handlers<State, State> = {
     const locale = form.get("locale");
     const headers = new Headers({ "Location": "/settings" });
     if (typeof locale === "string") {
-      setCookie(headers, {name: "locale", value: locale, maxAge: 60 * 60 * 24 * 365});
+      setCookie(headers, {
+        name: "locale",
+        value: locale,
+        maxAge: 60 * 60 * 24 * 365,
+      });
     }
-    return new Response("", {status: 303, headers});
+    return new Response("", { status: 303, headers });
   },
 };
 
@@ -23,12 +28,10 @@ export default function SettingsPage(props: PageProps<State>) {
   return (
     <div class="p-4 mx-auto max-w-screen-md">
       <h1 class="text-5xl mt-12 font-bold">Settings</h1>
-      <p class="mt-4">Your current locale is <b>{locales[0]}</b></p>
-      <form method="post" class="space-x-2">
-        <label htmlFor="locale">Locale</label>
-        <input type="text" name="locale" id="locale" class="border px-2 py-1" />
-        <button type="submit" class="px-2 py-2 bg-blue(500 hover:700 disabled:200) text-white font-medium">Save</button>
-      </form>
+      <p class="mt-4">
+        Your current locale is <b>{locales[0]}</b>
+      </p>
+      <LocaleSelector />
     </div>
   );
 }
